5ea8419a1bccb71046050e3e0d748ff529535bc5
Petr Yakovlev <yakovlev123peter@gmail.com>
2021-08-02 00:18:52 +0500

    ya v kilke

--- server.js
+++ server.js
@@ -58,8 +58,7 @@ app.get("/tester", (req, res) => {
 })
 
 
-
-app.listen(processs.env.PORT, function() {
+app.listen(process.env.PORT, function() {
     console.log(`Running on http://localhost:${PORT}`);
     connectDb().then(() => {
       console.log("MongoDb connected");
