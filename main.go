96fd74acff8721cacb4721483a57835b55428627
Petr Yakovlev <yakovlev123peter@gmail.com>
2021-08-05 23:37:22 +0500

    Fuck JS all the homies hate JS I ping mongoDB

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello i am server")
+	// Replace the uri string with your MongoDB deployment's connection string.
+	uri := "mongodb+srv://Hussar:Hussar1@hussarcluster.cokdm.mongodb.net/test"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
 }
