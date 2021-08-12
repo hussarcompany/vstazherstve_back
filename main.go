98f1320ee282ea0832d3f63ffbe24749671aeaa7
FeelinItNowMrKrabs <yakovlev123peter@gmail.com>
2021-08-12 20:11:29 +0500

    Vstaz 58 (#10)
    * Added file structure and /user endpoint for user creation in mongo
    
    * Remove useless files (+3 squashed commits)
    Squashed commits:
    [68fabd1] Server close on HTTP error (if not connecting to mongo)
    [e970ec7] Added get all users endpoint and get user by id endpoint
    [fdced36] Fixed initiazation of client bug and tidy go.mod

--- main.go
+++ main.go
@@ -1,33 +1,101 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/hussar_company/vstazherstve_back/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+var client *mongo.Client
+
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var user models.User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("abobus").Collection("dudes")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var users []models.User
+	collection := client.Database("abobus").Collection("dudes")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message:"` + err.Error() + `"}`))
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var user models.User
+		cursor.Decode(&user)
+		users = append(users, user)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message:"` + err.Error() + `"}`))
+		return
+	}
+	json.NewEncoder(response).Encode(users)
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func FindUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user models.User
+	collection := client.Database("abobus").Collection("dudes")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, models.User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message:"` + err.Error() + `"}`))
+	}
+	json.NewEncoder(response).Encode(user)
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	fmt.Println("Starting application")
+	var err error
+	client, err = mongo.NewClient(options.Client().ApplyURI("mongodb+srv://Hussar:Hussar1@hussarcluster.cokdm.mongodb.net/test"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	router.Run("localhost:8080")
+	fmt.Println(models.Herro())
+
+	fmt.Println(databases)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/makeuser", CreateUserEndpoint).Methods("POST")
+	router.HandleFunc("/users", GetUsersEndpoint).Methods("GET")
+	router.HandleFunc("/finduser/{id}", FindUserEndpoint).Methods("GET")
+
+	httpError := http.ListenAndServe(":8000", router)
+	if httpError != nil {
+		log.Println("While serving HTTP error: ", httpError)
+	}
 }
